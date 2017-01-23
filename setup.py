18db7de401b5aeab775c686899c306eadef92c08
Asif Mahmud Shimon <ams.eee09@gmail.com>
2017-01-23 20:04:16 +0600

    version 0.9.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='pyramid_runner',
-    version='0.0.1',
+    version='0.9.0',
     author='Asif Mahmud Shimon',
     email='ams.eee09@gmail.com',
     packages=find_packages(),
